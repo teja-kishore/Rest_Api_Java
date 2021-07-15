79afd372d3ff697e13436b825d5dce2ab7471074
teja-kishore <76863392+teja-kishore@users.noreply.github.com>
2021-07-15 10:28:22 +0530

    Update StudentDetailsController.java

--- src/main/java/com/example/demo/StudentDetailsController.java
+++ src/main/java/com/example/demo/StudentDetailsController.java
@@ -25,7 +25,7 @@ public class StudentDetailsController {
         return repository.save(newStudentDetails);
     }
     @PutMapping("/StudentDetails/{id}")
-    StudentDetails replaceEmployee(@RequestBody StudentDetails newStudentDetails, @PathVariable Long id) {
+    StudentDetails replaceStudentDetails(@RequestBody StudentDetails newStudentDetails, @PathVariable Long id) {
 
         return repository.findById(id)
                 .map(StudentDetails -> {
